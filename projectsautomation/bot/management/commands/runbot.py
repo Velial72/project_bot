f2904b48467d0fdbfac3a2f8c831e8b7e3abdd66
Velial72 <126498671+Velial72@users.noreply.github.com>
2023-10-28 12:41:08 +0300

    Update runbot.py
    добавил ветку времени

--- projectsautomation/bot/management/commands/runbot.py
+++ projectsautomation/bot/management/commands/runbot.py
@@ -17,50 +17,41 @@ class Command(BaseCommand):
 
     @bot.message_handler(commands=['start'])
     def start(message):
-        markup = types.InlineKeyboardMarkup(row_width=2)
+        markup = types.InlineKeyboardMarkup(row_width=1)
+        item1 = types.InlineKeyboardButton('Записаться на проект', callback_data='sing_in')
+        item2 = types.InlineKeyboardButton('Связаться с ПМ', callback_data='message_to_pm')
+        item3 = types.InlineKeyboardButton('Информация о проекте', callback_data='project_info')
+        markup.add(item1, item2, item3)
+        bot.send_message(message.chat.id, text=f'\nКакой у тебя вопрос?', reply_markup=markup)
 
-        item1 = types.InlineKeyboardButton('Ученик', callback_data='student')
-        item2 = types.InlineKeyboardButton('ПМ', callback_data='pm')
-        markup.add(item1, item2)
 
-        bot.send_message(message.chat.id, '\nПривет!\nСкажи кто ты?', reply_markup=markup)
-
-
-    @bot.message_handler(content_types=['text'])
+    @bot.message_handler(commands=['pm'])
     def set_name_project(message):
-        markup = types.InlineKeyboardMarkup(row_width=2)
-        item1 = types.InlineKeyboardButton('Назад', callback_data='pm')
-        item2 = types.InlineKeyboardButton('Создать проект', callback_data='create_project')
-        markup.add(item1, item2)
-
-        bot.send_message(message.chat.id, message.text, reply_markup=markup)
+        if message.from_user.id == 66907613:
+            markup = types.InlineKeyboardMarkup(row_width=1)
+            item1 = types.InlineKeyboardButton('Изменить время', callback_data='change_time')
+            item2 = types.InlineKeyboardButton('Инфа о командах', callback_data='team_info')
+            item3 = types.InlineKeyboardButton('Создать доску', callback_data='create_project')
+            markup.add(item1, item2, item3)
+            bot.send_message(message.chat.id, message.text, reply_markup=markup)
+        else:
+            bot.send_message(message.chat.id, text=f'\nТЕБЕ СЮДА НЕЛЬЗЯ!!!\n\nНажми "/start"')
 
 
     @bot.callback_query_handler(func=lambda call: True)
     def callback(call):
         if call.message:
-            if call.data == 'pm':
+            if call.data == 'change_time':
                 markup = types.InlineKeyboardMarkup(row_width=1)
-                item1 = types.InlineKeyboardButton('Изменить время', callback_data='change_time')
-                item2 = types.InlineKeyboardButton('Инфа о командах', callback_data='team_info')
-                item3 = types.InlineKeyboardButton('Создать доску', callback_data='trello')
+                item1 = types.InlineKeyboardButton('10:00 - 14:00', callback_data='pm_time#1')
+                item2 = types.InlineKeyboardButton('14:00 - 18:00', callback_data='pm_time#2')
+                item3 = types.InlineKeyboardButton('18:00 - 22:00', callback_data='pm_time#3')
                 markup.add(item1, item2, item3)
-                bot.edit_message_text(chat_id=call.message.chat.id,
-                                      message_id=call.message.id, text=f'\nЧто делаем?',
-                                      reply_markup=markup)
-
-            elif call.data == 'change_time':
-                markup = types.InlineKeyboardMarkup(row_width=1)
-                item1 = types.InlineKeyboardButton('10:00 - 14:00', callback_data='time#1')
-                item2 = types.InlineKeyboardButton('14:00 - 18:00', callback_data='time#2')
-                item3 = types.InlineKeyboardButton('18:00 - 22:00', callback_data='time#3')
-                item4 = types.InlineKeyboardButton('Назад', callback_data='pm')
-                markup.add(item1, item2, item3, item4)
                 bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.id,
                                       text=f'\nКогда тебе удобно взять учеников?',
                                       parse_mode='Markdown', reply_markup=markup)
 
-            elif 'time' in call.data:
+            elif 'pm_time' in call.data:
                 markup = types.InlineKeyboardMarkup(row_width=1)
                 flag = call.data.split('#')[1]
                 if flag == '1':
@@ -78,21 +69,10 @@ class Command(BaseCommand):
             elif call.data == 'team_info':
                 markup = types.InlineKeyboardMarkup(row_width=1)
                 item1 = types.InlineKeyboardButton('Команда 1', callback_data='---')
-                item2 = types.InlineKeyboardButton('Назад', callback_data='pm')
-                markup.add(item1, item2)
+                markup.add(item1)
                 bot.edit_message_text(chat_id=call.message.chat.id,
                                       message_id=call.message.id, text='\nТут будет инфа о командах',
                                       reply_markup=markup)
-
-            elif call.data == 'trello':
-                markup = types.InlineKeyboardMarkup(row_width=1)
-                item1 = types.InlineKeyboardButton('Назад', callback_data='pm')
-                markup.add(item1)
-
-                bot.edit_message_text(chat_id=call.message.chat.id,
-                                      message_id=call.message.id, text='\n Введите название проекта',
-                                      reply_markup=markup)
-
 
             elif call.data == 'create_project':
                 markup = types.InlineKeyboardMarkup(row_width=1)
@@ -108,36 +88,67 @@ class Command(BaseCommand):
                                       message_id=call.message.id, text='\n Доска создана',
                                       reply_markup=markup)
 
-            elif call.data == 'student':
+            elif call.data == 'sing_in':
                 markup = types.InlineKeyboardMarkup(row_width=1)
-                item1 = types.InlineKeyboardButton('Связаться с ПМ', callback_data='message_to_pm')
-                item2 = types.InlineKeyboardButton('Информация о проекте', callback_data='project_info')
-                markup.add(item1, item2)
-                bot.edit_message_text(chat_id=call.message.chat.id,
-                                      message_id=call.message.id, text=f'\nКакой у тебя вопрос?',
-                                      reply_markup=markup)
+                item1 = types.InlineKeyboardButton('10:00-14:00', callback_data='student_time#1')
+                item2 = types.InlineKeyboardButton('14:00 - 18:00', callback_data='student_time#2')
+                item3 = types.InlineKeyboardButton('18:00 - 22:00', callback_data='student_time#3')
+                markup.add(item1, item2, item3)
+                bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.id,
+                                      text='\nКогда тебе удобно участвовать в проекте?',
+                                      parse_mode='Markdown', reply_markup=markup)
 
+            elif 'student_time' in call.data:
+                markup = types.InlineKeyboardMarkup(row_width=6)
+                flag = call.data.split('#')[1]
+                if flag == '1':
+                    time = '10:00 - 14:00'
+                    item1 = [types.InlineKeyboardButton(f'{hour + 1}:00', callback_data=f'entry#{hour + 1}:00') for hour
+                             in range(9, 13)]
+                    item2 = [types.InlineKeyboardButton(f'{hour + 1}:30', callback_data=f'entry#{hour + 1}:30') for hour
+                             in range(9, 13)]
+                elif flag == '2':
+                    time = '14:00 - 18:00'
+                    item1 = [types.InlineKeyboardButton(f'{hour + 1}:00', callback_data=f'entry#{hour + 1}:00') for hour
+                             in range(13, 17)]
+                    item2 = [types.InlineKeyboardButton(f'{hour + 1}:30', callback_data=f'entry#{hour + 1}:30') for hour
+                             in range(13, 17)]
+                else:
+                    time = '18:00 - 22:00'
+                    item1 = [types.InlineKeyboardButton(f'{hour + 1}:00', callback_data=f'entry#{hour + 1}:00') for hour
+                             in range(17, 21)]
+                    item2 = [types.InlineKeyboardButton(f'{hour + 1}:30', callback_data=f'entry#{hour + 1}:30') for hour
+                             in range(17, 21)]
+                item3 = types.InlineKeyboardButton('Назад', callback_data='sing_in')
+                markup.add(*item1)
+                markup.add(*item2)
+                markup.add(item3)
+                bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.id,
+                                      text='\nВыбери удобное время',
+                                      parse_mode='Markdown', reply_markup=markup)
+
+            elif 'entry' in call.data:
+                flag = call.data.split('#')[1]
+                bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.id,
+                                      text=f'\nТы записан на {flag}.\nПозже с тобой свяжется ПМ',
+                                      parse_mode='Markdown')
+                
             elif call.data == 'message_to_pm':
                 markup = types.InlineKeyboardMarkup(row_width=1)
                 item1 = types.InlineKeyboardButton('Отправить', callback_data='---')
-                item2 = types.InlineKeyboardButton('Назад', callback_data='student')
-                markup.add(item1, item2)
+                markup.add(item1)
                 bot.edit_message_text(chat_id=call.message.chat.id,
                                       message_id=call.message.id, text='\nНапиши сообщение ПМу, он когда-нибудь ответит.',
                                       reply_markup=markup)
 
             elif call.data == 'project_info':
-                markup = types.InlineKeyboardMarkup(row_width=1)
-                item1 = types.InlineKeyboardButton('Назад', callback_data='student')
-                markup.add(item1)
                 bot.edit_message_text(chat_id=call.message.chat.id,
                                       message_id=call.message.id, text='\nТвой проект: "Название"\n'
                                                                        'Команда: \n"участник 1"'
                                                                        '\n"участник 2"'
                                                                        '\n"участник 3"\n'
                                                                        'Ваш ПМ: "Егор"\n'
-                                                                       'Созвон в такое-то время.',
-                                      reply_markup=markup)
+                                                                       'Созвон в такое-то время.')
 
 
     def handle(self, *args, **options):
